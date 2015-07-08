7a5a1f1def51e10f584b20240a57990208330d79
Troy Scott <troys2005@gmail.com>
2015-07-07 21:35:09 -0700

    Commit 3-1\nAdded basic posts list template and static data

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,22 @@
+
+var postsData = [
+
+{
+	title: 'Introducing Telescope',
+	url: 'http://sachagreif.com/introducing-telecscope'
+
+},
+{
+	title: 'Meteor',
+	url: 'http://meteor.com'
+
+}
+
+
+];
+
+Template.postsList.helpers({
+	posts: postsData
+
+});
+
